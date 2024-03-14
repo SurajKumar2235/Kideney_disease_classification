1f73520ce68a258d98954068b5a1ce95785047e0
SurajKumar2235 <botvirus2235@gmail.com>
2024-03-14 20:20:43 +0530

    data ingestions

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from cnnClassifier import logger
-
+from cnnClassifier.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 
 logger.info("Welcome to App Logs")
 
+STAGE_NAME="DATA ingestion stage"
 
 
+try:
+    logger.info(f'>>>>>>>>stage{STAGE_NAME} started<<<<<<<<<<<')
+    obj=DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>>>stage {STAGE_NAME} compleated<<<<<\n\nx==========x')
+except Exception as e:
+    logger.exception(e)
+    raise e
