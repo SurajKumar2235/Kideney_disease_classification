14929eb43a0a46518936d502903ac85e86c20a4e
SurajKumar2235 <botvirus2235@gmail.com>
2024-03-24 14:22:53 +0530

    updated configuration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage02_base_model import PrepareBaseModelTrainingPipeline
 
 logger.info("Welcome to App Logs")
 
@@ -9,6 +10,19 @@ STAGE_NAME="DATA ingestion stage"
 try:
     logger.info(f'>>>>>>>>stage{STAGE_NAME} started<<<<<<<<<<<')
     obj=DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>>>stage {STAGE_NAME} compleated<<<<<\n\nx==========x')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME="prepare base model"
+
+
+try:
+    logger.info(f'>>>>>>>>stage{STAGE_NAME} started<<<<<<<<<<<')
+    obj=PrepareBaseModelTrainingPipeline()
     obj.main()
     logger.info(f'>>>>>>stage {STAGE_NAME} compleated<<<<<\n\nx==========x')
 except Exception as e:
